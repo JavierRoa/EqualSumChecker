077d7fa7b53f6ab76a602e4b8135abfdeea98e7b
Javier Roa <javierroa159@gmail.com>
2024-03-10 19:15:28 -0300

    Initial commit

---
+++ src/main/java/EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1)); // false
+        System.out.println(hasEqualSum(1, 1, 2)); // true
+        System.out.println(hasEqualSum(1, -1, 0)); // true
+
+    }
+    public static boolean hasEqualSum(int primerNumero, int segundoNumero, int tercerNumero) {
+        return (primerNumero+segundoNumero) == tercerNumero;
+    }
+}
